e8fde3fa102d87d994dd81126ed4440eb8ceddf5
gawao <gawao57@gmail.com>
2021-04-20 00:02:08 +0900

    p.152-153 deriveマクロ形式

--- src/main-derive.rs
+++ src/main-derive.rs
@@ -1,3 +1,30 @@
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+
+// 自作の構造体
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
+    let opts = Opts::parse();
+
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose)
 }
